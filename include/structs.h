d6944a0cecc1be4c6ecd00ead6fae1a4fee6f88f
hosein <bahadorm@informatik.uni-freiburg.de>
2020-03-31 23:14:26 +0200

    add more functionality to date class, add some functions
    GetFreeDays() calculates how many days are available for scheduling. Date class is more or less finished

--- include/structs.h
+++ include/structs.h
@@ -1,9 +1,96 @@
+#include <iostream>
+#include <cstdlib>
+
+#include <vector>
+#include <unordered_map>
+
+
+#include <string>
+#include <sstream>
+#include <iomanip>
+
 #ifndef __WS_STRUCTS_H__
 #define __WS_STRUCTS_H__
 
-struct Date {
-	unsigned char month;
-	unsigned char day;
+enum WeekDay : int {
+	MON = 0,
+	TUE = 1,
+	WED = 2,
+	THU = 3,
+	FRI = 4,
+	SAT = 5,
+	SUN = 6
 };
 
+namespace stat
+{
+	static const unsigned char months_norm [] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	static const unsigned char months_leap [] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	static const std::unordered_map<std::string, WeekDay>  weekday({{"monday", WeekDay::MON}, {"tuesday", WeekDay::TUE},
+																    {"wednesday", WeekDay::WED}, {"thursday", WeekDay::THU},
+																    {"friday", WeekDay::FRI}, {"saturday", WeekDay::SAT},
+																    {"sunday", WeekDay::SUN}});
+}
+class Date 
+{
+public:
+  Date() : day_(1), month_(1) {}
+  Date(const std::string &date_str) {
+	  if (date_str.length() != 5) {
+		  std::cout << "ERROR: expected date format DD.MM" << std::endl;
+		  std::exit(EXIT_FAILURE);
+	  } else {
+			try {
+				day_ = std::stoi(date_str.substr(0, 2));
+		  		month_ = std::stoi(date_str.substr(3, 2));
+			} catch (std::invalid_argument const &e) {
+				std::cout << "ERROR: expected date format DD.MM" << std::endl;
+				std::exit(EXIT_FAILURE);
+			}
+	  }
+	}
+	Date(unsigned char day, unsigned char month) : day_(day), month_(month) {
+		if (day_ == 0 || month_ == 0 || month_ > 12 || day_ > stat::months_norm[month_]) {
+			std::cout << "ERROR: invalid date DD.MM" << ToString() << std::endl;
+			std::exit(EXIT_FAILURE);
+		}
+	}
+	std::string ToString() {
+		std::stringstream ss;
+		ss << std::setw(2) << std::setfill('0') << day_ << "." << month_;
+		return ss.str();
+	}
+	Date& operator++() {
+		if (++day_ > stat::months_norm[month_]) {
+			month_ = (month_ + 1) % 12;
+			day_ = 1;
+		}
+		return *this;
+	}
+	bool operator==(const Date& other) {
+		return day_ == other.day_ && month_ == other.month_;
+	}
+	bool operator!=(const Date& other) {
+		return day_ != other.day_ || month_ != other.month_;
+	}
+	bool operator<(const Date& other) {
+		if (month_ < other.month_ || (month_ == other.month_ && day_ < other.day_))
+			return true;
+		return false;
+	}
+	bool operator<=(const Date& other) {
+		if (month_ < other.month_ || (month_ == other.month_ && day_ <= other.day_))
+			return true;
+		return false;
+	}
+private:
+	unsigned char day_;
+	unsigned char month_;
+};
+
+/* getting rid of ccls warnings */
+void Placeholder() {
+	(void)stat::months_norm;
+	(void)stat::months_leap;
+}
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,61 @@
 
 #include "structs.h"
 
+
+// what to skip
 bool skip_weekends;
 std::vector<Date> skip_dates;
+// not implemented yet
+bool leap_year;
+// used for skipping weekends
+WeekDay starting_weekday;
+Date start_date, end_date;
+// amount of hours to fill
+float hours;
+// time of day related variables
+float day_start, day_end, lunch_break_start, lunch_break_end;
+/* initializes the global variables. to be replaced by a yaml read  */
+void Init() {
+	skip_weekends = true;
+	leap_year = false;
+	start_date = Date("16.03");
+	end_date = Date("15.04");
+	hours = 60.0f;
+	starting_weekday = stat::weekday.at("monday");
+
+	day_start = 9.0f;
+	day_end = 20.0f;
+	lunch_break_start = 13.0f;
+	lunch_break_end = 14.5f;
+}
+unsigned int GetFreeDays() {
+	unsigned int free_days = 0;
+	size_t skip_index = 0;
+	auto weekday = starting_weekday;
+	Date current_date(start_date);
+	while (current_date != end_date) {
+		// if not skipping weekends
+		if (!skip_weekends && (weekday == WeekDay::SAT || weekday == WeekDay::SUN))
+			free_days++;
+		else {
+			// if skipping this date
+			if (skip_index < skip_dates.size() && current_date == skip_dates[skip_index])
+				skip_index++;
+			else
+				free_days++;
+		}
+		weekday = static_cast<WeekDay>((static_cast<unsigned char>(weekday) + 1) % 6);
+		++current_date;
+	}
+	return free_days;
+}
+
+int main (int argc, char** argv)
+{
+	srand(1984);
+	float filled_hours = 0.0f;
+
+	while (filled_hours != hours) {
+
+	}
+}
