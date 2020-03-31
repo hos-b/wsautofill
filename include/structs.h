7403cf40aa1950fb7221332abb67c74491c93691
hosein <bahadorm@informatik.uni-freiburg.de>
2020-03-31 18:46:19 +0200

    initial commit

---
+++ include/structs.h
@@ -0,0 +1,9 @@
+#ifndef __WS_STRUCTS_H__
+#define __WS_STRUCTS_H__
+
+struct Date {
+	unsigned char month;
+	unsigned char day;
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <xlnt/xlnt.hpp>
+
+#include "structs.h"
+
+bool skip_weekends;
+std::vector<Date> skip_dates;
